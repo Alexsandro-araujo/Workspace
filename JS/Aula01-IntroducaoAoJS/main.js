db003c267528138dc80e9b349a61c83b555d0152
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2023-10-19 21:47:50 -0300

    subindo notas

--- JS/Aula01-IntroducaoAoJS/main.js
+++ JS/Aula01-IntroducaoAoJS/main.js
@@ -2,4 +2,5 @@ var nome= prompt("Digite o seu nome", nome)
 
 
 
-alert("olá,  É um prazer em conhece-lo (a) , " + nome)
+alert(`olá ${nome}, o seu nome possui ${nome.length} `)
+                                       //AQUI ESTAMOS COLOCANDO NA TELA DO USUARIO A QUANTIDADE DE LETRAS TEM O SEU NOME 
---
+++ JS/aula03-Strings/main.js
@@ -0,0 +1,33 @@
+var numero1 = 2;
+var numero3 = 220;
+console.log(`a soma entre os valores  ${numero1}  e ${numero3} é de:  ${numero1 + numero3} `) 
+console.clear()
+
+var nome = "DEPRESSION"
+
+console.log(" a posição da letra é " + nome.charAt(2), nome.charAt(1))
+console.clear()
+
+var texto = "Alexsandro"
+
+console.log(` seu texto, ${texto} tem  ${texto.length} letras` )
+
+console.clear()
+
+var nome = "alexsaNDRO"
+console.log(nome.toLocaleLowerCase())
+console.log(nome.toUpperCase())
+
+console.clear()
+
+
+var sub = "Mozilla"
+
+console.log(sub.substring(1, 5))
+
+console.clear()
+
+
+var retirandoEspacos = "        A VIDA É UMA UVA        "
+
+console.log(retirandoEspacos.trim())
