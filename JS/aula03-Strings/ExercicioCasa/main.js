3cdcc613351aec83f2c6948abeaa542f74b58462
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2023-10-22 18:53:38 -0300

    exercicio 3

---
+++ JS/aula03-Strings/ExercicioCasa/main.js
@@ -0,0 +1,10 @@
+var exercicio = "Estamos Aprendendo JavaScript e seremos futuros Devs"
+
+console.log( 
+    "O caracter correspondente é " + exercicio.charAt(19))
+console.log(
+    "O tamanho total dessa string é de " + exercicio.length)
+console.log(
+    "A string em Maiusculo " + exercicio.toUpperCase())
+console.log(
+   "Somos todos : " + exercicio.substring(47,52))
