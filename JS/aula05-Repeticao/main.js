ab0a49431a9186f687f00482d3e180593d91a55a
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2023-10-25 20:52:06 -0300

    povo doido

---
+++ JS/aula05-Repeticao/main.js
@@ -0,0 +1,8 @@
+// Laço while
+
+let contador = 1;
+
+while(contador <= 10){ //true
+    console.log(`O numero do contador é ${contador}`)
+    contador++; //estamos adicionando um numero a mais (++) incrementando
+}
