c812449b3611d576cf584e96246434dc84bbf5b6
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2023-10-25 21:50:59 -0300

    socorro

---
+++ JS/Aula04-Condicionais/Exerciciocasa/main.js
@@ -0,0 +1,8 @@
+var numero1 = 4
+var numero2 = 2
+
+if (numero1 > numero2) {
+    console.log(`O ${numero1} é maior que o ${numero2}`)
+} else {
+    console.log(`o Numero ${numero2} é menor que ${numero1}`)
+}
---
+++ JS/aula05-Repeticao/ExercicioCasa/main.js
@@ -0,0 +1,9 @@
+// Laço while
+
+let contador = 1;
+let tabu = 0;
+
+while(contador >= 0){ //true
+    console.log(`o valor da tabuada é ${contador}`)
+        ; //estamos adicionando um numero a mais (++) incrementando
+}
