cee3d605df6eb395f7cd2949c2b27a1af6c7ddc1
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2023-10-26 21:54:46 -0300

    sofrendo com os lindu dos ARRAY

--- JS/Aula06-Array/main.js
+++ JS/Aula06-Array/main.js
@@ -61,9 +61,24 @@ let novaInformalcao = numArray.push("Kiwi")
 console.log(numArray)
 console.log(novaInformalcao)
 console.clear()
-//Metodo ADD nova informação no final do array
+//Metodor Retirar uma informação no inicio do array
 var numArray = [2,3,6,7,45,86]
 console.log(numArray)
-let retiraInicio = numArray.shift() // 
+let retiraInicio = numArray.shift() // iremos retirar o 2
 console.log(numArray)
 console.log(retiraInicio)
+console.clear()
+
+//Metodo ADD no inicio
+var numArray = [2,3,6,7,45,86]
+console.log(numArray)
+let inserirInicio = numArray.unshift("Pipoca") //Iremos acrescentar a pipoca no nosso inicio do array
+console.log(numArray)
+console.log(inserirInicio)
+console.clear()
+
+//Metodo Delete serve para remover um elento da posição deseja no array
+var numArray = [2,3,6,7,45,86]
+console.log(numArray)
+delete numArray[3]
+console.log(numArray)
