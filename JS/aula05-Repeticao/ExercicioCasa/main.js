86bbedec7cd8329181abbdadcc7b778c3b00b368
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2023-10-29 17:30:12 -0300

    certo

--- JS/aula05-Repeticao/ExercicioCasa/main.js
+++ JS/aula05-Repeticao/ExercicioCasa/main.js
@@ -1,7 +1,6 @@
-let tabuada = 2
-let mul = tabuada * contador
+let tabuada = 5
 let contador = 0
-while(contador <=10){
-    console.log(tabuada + " * " + contador + " = " + tabuada * contador + 1)
+while(contador <= 10){
+    console.log(tabuada + " * " + contador + " = " + tabuada * contador)
     contador = contador + 1
 }
