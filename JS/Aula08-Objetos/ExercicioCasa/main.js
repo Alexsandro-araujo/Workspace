22b19a14b9562309d468ab2292abf3dc4dcdc57b
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2023-11-05 18:49:33 -0300

    js

--- JS/Aula08-Objetos/ExercicioCasa/main.js
+++ JS/Aula08-Objetos/ExercicioCasa/main.js
@@ -1,3 +1,27 @@
 function somaNumeros(num1 = 0, num2 = 0, num3 = 0) {
     return num1 + num2 + num3;
 }
+
+function alertCookie(){
+    alert("Voce quer um cookie?")
+}
+
+console.log("Você quer um cookie?")
+
+const hello = (alertSucesso) => { 
+    alert("Mensagem gerada com sucesso")
+    return 'Mensagem gerada com sucesso';
+    
+};
+
+console.log(hello())
+
+
+const notebook = { 
+    marca: 'Dell', 
+    modelo: 'I5 11Geração', 
+    cor: 'Branco',
+    memoria: '500gb',
+    memoriaRam: '16gb'
+}
+console.log(notebook)
