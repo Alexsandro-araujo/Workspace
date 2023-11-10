0a8dc816a9509afd2364d2b9f0028915c00cf51a
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2023-11-09 21:29:35 -0300

    subindo

---
+++ JS/Aula12-DOM-Parte2/ExercicioCasa/main.js
@@ -0,0 +1,13 @@
+let bobtao = document.getElementsByClassName('B_12')
+document.body.style.border = 'solid 2px #D9BC66'
+document.body.style.margin = '.5rem'
+document.body.style.backgroundColor = '#BFBFBF'
+document.body.style.padding = '.7rem'
+
+
+
+
+
+
+
+
